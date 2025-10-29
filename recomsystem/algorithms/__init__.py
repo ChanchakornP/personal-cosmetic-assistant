eb24ee5ff46ffd23c378ec66078dab13a66f32b9
Ethan Ma <homa0510@uni.sydney.edu.au>
2025-10-30 06:41:36 +1100

    Updated list of algorithms

--- recomsystem/algorithms/__init__.py
+++ recomsystem/algorithms/__init__.py
@@ -1,2 +1,8 @@
-# Algorithms package
+"""Algorithms package for recommendation strategies.
+
+Available strategies:
+- content_based: Rule-based content matching
+- popularity: Popularity/availability heuristic
+- hybrid: Weighted combination of content and popularity
+"""
 
