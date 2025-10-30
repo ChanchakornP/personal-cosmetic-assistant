12fc0e4503b8af4fe76fe3dae38dacc893b0941f
ChanchakornP <gamechanchakorn@gmail.com>
2025-10-30 16:37:47 +1100

    containerized

---
+++ payment/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO accounts (balance) VALUES (1500.00);
+INSERT INTO accounts (balance) VALUES (320.50);
+INSERT INTO accounts (balance) VALUES (9800.75);
+
+INSERT INTO transactions (from_account_id, to_account_id, amount) VALUES (1, 2, 120.00);
+INSERT INTO transactions (from_account_id, to_account_id, amount) VALUES (2, 3, 88.50);
+INSERT INTO transactions (from_account_id, to_account_id, amount) VALUES (3, 1, 520.00);
+
+
