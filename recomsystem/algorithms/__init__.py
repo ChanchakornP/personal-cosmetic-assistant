3a88a556aa0eabfcfcd95b4eb1cd80f26b4a684c
ChanchakornP <gamechanchakorn@gmail.com>
2025-11-01 19:22:16 +1100

    remove previous recomsystem

--- recomsystem/algorithms/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Algorithms package for recommendation strategies.
-
-Available strategies:
-- content_based: Rule-based content matching
-- popularity: Popularity/availability heuristic
-- hybrid: Weighted combination of content and popularity
-"""
-
